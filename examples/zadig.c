2c47f310dbce59feea00198722692db5125cd799
adamlee1104 <148270015+adamlee1104@users.noreply.github.com>
2024-09-12 14:42:50 +0800

    Update zadig.c

--- examples/zadig.c
+++ examples/zadig.c
@@ -82,10 +82,10 @@ float fScale = 1.0f;
 WORD application_version[4];
 char app_dir[MAX_PATH], driver_text[64];
 char szFolderPath[MAX_PATH];
-#const char* driver_display_name[WDI_NB_DRIVERS] = { "WinUSB", "libusb-win32", "libusbK", "USB Serial (CDC)", "Custom (extract only)" };
-#const char* driver_name[WDI_NB_DRIVERS] = { "WinUSB", "libusb0", "libusbK", "usbser", "custom" };
-const char* driver_display_name[WDI_NB_DRIVERS] = { "libusbK" };
-const char* driver_name[WDI_NB_DRIVERS] = { "libusbK" };
+const char* driver_display_name[WDI_NB_DRIVERS] = { "WinUSB", "libusb-win32", "libusbK", "USB Serial (CDC)", "Custom (extract only)" };
+const char* driver_name[WDI_NB_DRIVERS] = { "WinUSB", "libusb0", "libusbK", "usbser", "custom" };
+//const char* driver_display_name[WDI_NB_DRIVERS] = { "libusbK" };
+//const char* driver_name[WDI_NB_DRIVERS] = { "libusbK" };
 struct wdi_options_create_list cl_options = { 0 };
 struct wdi_options_prepare_driver pd_options = { 0 };
 struct wdi_options_install_cert ic_options = { 0 };
@@ -272,8 +272,8 @@ void __cdecl notification_delay_thread(void* param)
 int get_driver_type(struct wdi_device_info* dev)
 {
 	int i;
-	#const char* libusb_name[] = { "WinUSB", "libusb0", "libusbK", "usbser" };
-	const char* libusb_name[] = { "libusbK" };
+	const char* libusb_name[] = { "WinUSB", "libusb0", "libusbK", "usbser" };
+	//const char* libusb_name[] = { "libusbK" };
 	const char* system_name[] = { "usbccgp", "usbstor", "uaspstor", "vusbstor", "etronstor", "hidusb",
 		// NOTE: The list of hubs below should match the one from libwdi.c
 		"usbhub", "usbhub3", "nusb3hub", "usbhub", "usbhub3", "usb3hub", "nusb3hub", "rusb3hub",
